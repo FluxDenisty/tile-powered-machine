cbc75589f0156977886e3372a1a6ad76c40d9897
Ryan <ryanbateman.uw@gmail.com>
2013-02-03 17:15:28 -0800

    Added grid drawing

--- game.py
+++ game.py
@@ -51,8 +51,20 @@ class Game:
 
     def drawBoard(self):
         global DRAW_OFFSET, window
-        box = (DRAW_OFFSET['x'], DRAW_OFFSET['y'], self.width * 50, self.height * 50)
+        box = list((DRAW_OFFSET['x'], DRAW_OFFSET['y'], self.width * 50, self.height * 50))
         window.fill(pygame.Color('grey'), box, 0)
+
+        box[2] = 50
+        box[3] = 50
+        for x in xrange(0, len(self.grid)):
+            box[1] = DRAW_OFFSET['y']
+            for y in xrange(0, len(self.grid[x])):
+                tile = self.grid[x][y]
+                if tile is None:
+                    pygame.draw.rect(window, pygame.Color('red'), box, 1)
+
+                box[1] += 50
+            box[0] += 50
 
     def drawHand(self):
         global DRAW_OFFSET, window
